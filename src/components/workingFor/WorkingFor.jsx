fe5452e2681f58f796f3bfc6bb2e835b48a213be
Hariom dangi <hdangi224@gmail.com>
2023-11-02 15:38:21 +0530

    changes

--- src/components/workingFor/WorkingFor.jsx
+++ src/components/workingFor/WorkingFor.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import styles from "./style.modul.css"
+import "./working.css"
 import hand from "../../assets/hand.png"
 import responsibility from "../../assets/responsibility.png"
 import setting from "../../assets/setting.png"
